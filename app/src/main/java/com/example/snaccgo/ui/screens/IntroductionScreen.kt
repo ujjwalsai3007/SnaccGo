f659f5b269709d6611ce218dca0cdb829a637841
Ujjwal sai <ujjwalsai501@gmail.com>
2025-04-07 11:51:48 +0530

    FireBase Done

---
+++ app/src/main/java/com/example/snaccgo/ui/screens/IntroductionScreen.kt
@@ -0,0 +1,17 @@
+package com.example.snaccgo.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun IntroductionScreen(){
+
+
+
+}
+
+@Composable
+@Preview(showSystemUi = true, showBackground = true)
+fun IntroductionScreenPreview(){
+    IntroductionScreen()
+}
